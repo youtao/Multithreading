d03daf4562433827fee8e00ba4e537a7662ab448
youtao <lytlytlc@gmail.com>
2016-08-23 10:06:47 +0800

    线程基础

---
+++ 001_线程基础/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _001_线程基础
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resource resource = new Resource();
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(e => // 竞争条件
+                {
+                    for (int j = 0; j < 1000000; j++)
+                    {
+                        resource.Count++;
+                        resource.Count--;
+                    }
+                });
+            }            
+
+            Thread.Sleep(2000);
+            Console.WriteLine(resource.Count);
+            Console.ReadKey();
+        }
+    }
+
+    public class Resource
+    {
+        public int Count { get; set; }
+    }
+}
