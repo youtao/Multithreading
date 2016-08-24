ff197297af96ff97af18fffed518dbf7fa66fb00
youtao <lytlytlc@gmail.com>
2016-08-24 13:06:57 +0800

    任务并行库

---
+++ 0041_使用TaskScheduler/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _0041_使用TaskScheduler
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSync_Click(object sender, RoutedEventArgs e)
+        {
+            ContentTextBlock.Text = string.Empty;
+            try
+            {
+                string resul = TaskMethod().Result;
+                ContentTextBlock.Text = resul;
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null) ContentTextBlock.Text = ex.InnerException.Message;
+            }
+        }
+
+        private void ButtonAsync_Click(object sender, RoutedEventArgs e)
+        {
+            ContentTextBlock.Text = string.Empty;
+            Mouse.OverrideCursor = Cursors.Wait;
+            Task<string> task = TaskMethod();
+                task.ContinueWith(t =>
+                {
+                    ContentTextBlock.Text = t.Exception.InnerException.Message;
+                    Mouse.OverrideCursor = null;
+                },
+                CancellationToken.None,
+                TaskContinuationOptions.OnlyOnFaulted,
+                TaskScheduler.FromCurrentSynchronizationContext()
+            );
+        }
+
+        private void ButtonAsyncOK_Click(object sender, RoutedEventArgs e)
+        {
+            ContentTextBlock.Text = string.Empty;
+            Mouse.OverrideCursor = Cursors.Wait;
+            Task<string> task = TaskMethod(TaskScheduler.FromCurrentSynchronizationContext());
+            task.ContinueWith(t =>
+                {
+                    Mouse.OverrideCursor = null;
+                },
+                CancellationToken.None,
+                TaskContinuationOptions.None,
+                TaskScheduler.FromCurrentSynchronizationContext()
+            );
+        }
+
+        Task<string> TaskMethod()
+        {
+            return TaskMethod(TaskScheduler.Default);
+        }
+
+        Task<string> TaskMethod(TaskScheduler scheduler)
+        {
+            Task delay = Task.Delay(TimeSpan.FromSeconds(5));
+            return delay.ContinueWith(e =>
+            {
+                string str = string.Format("Task is runing on a thread id {0}. Is thread  pool thread:{1}",
+                    Thread.CurrentThread.ManagedThreadId,
+                    Thread.CurrentThread.IsThreadPoolThread);
+                ContentTextBlock.Text = str;
+                return str;
+            }, scheduler);
+        }
+    }
+}
---
+++ 004_任务并行库/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _004_任务并行库
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Task<int> task = Task.Run(() => TaskMethod());
+
+            //    var result = task.Result;
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.InnerException.Message);
+            //}
+            //Thread.Sleep(3000);
+
+            var t1 = new Task<int>(TaskMethod);
+            var t2 = new Task<int>(TaskMethod);
+            var complexTask = Task.WhenAll(t1, t2);
+            complexTask.ContinueWith(e =>
+            {                
+                Console.WriteLine("两个任务完成");
+            }, TaskContinuationOptions.OnlyOnFaulted); // 有异常时才执行
+            t1.Start();
+            t2.Start();            
+
+            Console.WriteLine("主线程运行完成");
+            Console.ReadKey();
+        }
+
+        static int TaskMethod()
+        {
+            Console.WriteLine("线程Id{0},是否在线程池中:{1}",
+                Thread.CurrentThread.ManagedThreadId,
+                Thread.CurrentThread.IsThreadPoolThread);
+            Thread.Sleep(2000);
+            //throw new Exception("自己发生的异常");
+            return 10;
+        }
+    }
+}
